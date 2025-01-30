c5dfca1993e26b0bade2b5859ee53c2ce8aef2a2
carol <carol@Carolina>
2025-01-30 10:45:22 -0500

    practice the method and the logic

--- src/Demo1/DemoTest5.java
+++ src/Demo1/DemoTest5.java
@@ -1,5 +1,25 @@
 package Demo1;
 
 public class DemoTest5 {
+	
+public static void main (String args[]) {
+	
+		
+		int age = 10;
+		
+		if(age < 5) {
+			System.out.println("Go to swimming lesson");
+			
+		}
+			
+			if (age == 10) {
+				System.out.println(" Go to aftershcool program");
+			}
+			
+			if (age > 8); {
+			System.out.println("Study Java");
+			}
+
+	}
 
 }
